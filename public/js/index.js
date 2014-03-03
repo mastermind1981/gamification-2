59be4cfc5270e665327a34a90d6c0c147c1d2e04
Jeremy Toussaint <jeremyt@uio.im>
2014-03-03 13:46:17 +0100

    added further database integration

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,12 @@
 gamififcationApp.controller('navigationCtrl', function($scope, $http, $q) {
 
+    $scope.testVar = "hello";
 
-    function requestHttpData(config) {
+        function requestHttpData(config) {
         var deferred = $q.defer();
         $http(config).success(function(data, status, textStatus, jqXHR) {
             deferred.resolve([data, status, textStatus, jqXHR]);
+            $scope.testVar = data;
         }).error(function(data){
             alert( "Request failed: " + data.message  );
             deferred.reject();
@@ -12,11 +14,8 @@ gamififcationApp.controller('navigationCtrl', function($scope, $http, $q) {
         return deferred.promise;
     }
 
-    function getData(url) {
+    $scope.getData = function(url) {
         var config = { method: "GET", url : url };
         return requestHttpData(config);
     }
-
-    $scope.testVar = getData("/testobj");
-
 });
