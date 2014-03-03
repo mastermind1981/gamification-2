4d3133220850c2125ee9e82ea51a2dc882fa2711
Jeremy Toussaint <jeremyt@uio.im>
2014-03-03 14:13:23 +0100

    facebook login

--- config.ru
+++ config.ru
@@ -1,4 +1,4 @@
 root = ::File.dirname(__FILE__)
 require ::File.join( root, 'web' )
-#require './env' if File.exists?('env.rb')
+require './env' if File.exists?('env.rb')
 run Gamification.new
--- web.rb
+++ web.rb
@@ -8,10 +8,13 @@ require 'fileutils'
 require 'securerandom'
 require 'logger'
 require 'bcrypt'
+require 'koala'
 
 
 class Gamification < Sinatra::Application
   include BCrypt
+
+  use Rack::Session::Cookie, secret: 'qwertyuiop123456asdfghjkl'
 
   set :environment, :production
 
@@ -30,8 +33,42 @@ class Gamification < Sinatra::Application
   end
 
   get '/' do
-    send_file File.join('public', 'index.html')
+    #send_file File.join('public', 'index.html')
+
+    if session['access_token']
+      'You are logged in! <a href="/logout">Logout</a>'
+      # do some stuff with facebook here
+      # for example:
+      # @graph = Koala::Facebook::GraphAPI.new(session["access_token"])
+      # publish to your wall (if you have the permissions)
+      # @graph.put_wall_post("I'm posting from my new cool app!")
+      # or publish to someone else (if you have the permissions too ;) )
+      # @graph.put_wall_post("Checkout my new cool app!", {}, "someoneelse's id")
+    else
+      '<a href="/login">Login</a>'
+    end
   end
+
+  get '/login' do
+    # generate a new oauth object with your app data and your callback url
+    session['oauth'] = Koala::Facebook::OAuth.new(ENV['APP_ID'], ENV['APP_SECRET'], "#{request.base_url}/callback")
+    # redirect to facebook to get your code
+    redirect session['oauth'].url_for_oauth_code()
+  end
+
+  get '/logout' do
+    session['oauth'] = nil
+    session['access_token'] = nil
+    redirect '/'
+  end
+
+  #method to handle the redirect from facebook back to you
+  get '/callback' do
+    #get the access token from facebook with your code
+    session['access_token'] = session['oauth'].get_access_token(params[:code])
+    redirect '/'
+  end
+
 end
 
 require_relative 'routes/init'
