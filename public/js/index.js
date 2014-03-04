e0940a413b7c763f7f739befa13cd00c8644e52b
Jeremy Toussaint <jeremyt@uio.im>
2014-03-04 15:57:26 +0100

    secured browsing based on fb access_token

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-gamififcationApp.controller('navigationCtrl', function($scope, $http, $q, gamificationData, $location) {
+gamififcationApp.controller('navigationCtrl', function($scope, $http, $q, gamificationFactory, $location) {
 
     $scope.first_name = "";
     $scope.last_name = "";
@@ -7,17 +7,23 @@ gamififcationApp.controller('navigationCtrl', function($scope, $http, $q, gamifi
     initIndexView();
 
     function initIndexView() {
-        console.log("initialising index ....");
-        var url = 'https://graph.facebook.com/me?access_token='+$location.search()['at'];
-        gamificationData.doGetURL(url).then(function (response) {
-            $scope.first_name = response[0].first_name;
-            $scope.last_name = response[0].last_name;
-            $scope.facebookId = response[0].id;
-        });
+        $scope.at = $location.search()['at'];
+
+        if($scope.at != undefined) {
+            console.log("initialising index ....");
+            var url = 'https://graph.facebook.com/me?access_token='+$scope.at;
+            gamificationFactory.doGetURL(url).then(function (response) {
+                $scope.first_name = response[0].first_name;
+                $scope.last_name = response[0].last_name;
+                $scope.facebookId = response[0].id;
+            });
+        }
+        else {
+            gamificationFactory.doLogOut();
+        }
     };
 
     $scope.logout = function() {
-        console.log("login out ....");
-        window.location.href = '/logout';
+        gamificationFactory.doLogOut();
     };
 });
